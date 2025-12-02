f946f38fabeb58bd8b22d6ce5ee68b7fed1e0229
Khushvakht799 <cybermind.jarvis@gmail.com>
2025-12-02 08:21:06 +0100

    Initial commit of Jarvis V_1

---
+++ run.ps1
@@ -0,0 +1,12 @@
+﻿Write-Host "Запуск Jarvis с Embedding-овым словарем..." -ForegroundColor Cyan
+Write-Host ""
+
+if (Test-Path "venv\Scripts\Activate.ps1") {
+    & "venv\Scripts\Activate.ps1"
+    python jarvis.py
+} else {
+    python jarvis.py
+}
+
+Read-Host "
+Нажмите Enter для выхода"
